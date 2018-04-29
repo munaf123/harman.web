477b73db614970560879a5dd76e8faf25626eedf
munaf123 <38830762+munaf123@users.noreply.github.com>
2018-04-29 14:57:14 +0530

    webapi project

---
+++ API/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace HarmanAssignment
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            var cors = new EnableCorsAttribute(
+            origins: "*",
+            headers: "*",
+            methods: "*");
+            // Web API configuration and services
+            config.EnableCors(cors);
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+        }
+    }
+}
---
+++ API/Controllers/AdressesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using HarmanAssignment.Models;
+
+namespace HarmanAssignment.Controllers
+{
+    public class AdressesController : ApiController
+    {
+        private HarmanDBEntities db = new HarmanDBEntities();
+
+        // GET: api/Adresses
+        public IQueryable<Adress> GetAdresses()
+        {
+            return db.Adresses;
+        }
+
+        // GET: api/Adresses/5
+        [ResponseType(typeof(Adress))]
+        public IHttpActionResult GetAdress(int id)
+        {
+            Adress adress = db.Adresses.Find(id);
+            if (adress == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(adress);
+        }
+
+        // PUT: api/Adresses/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutAdress(int id, Adress adress)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != adress.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(adress).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AdressExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Adresses
+        [ResponseType(typeof(Adress))]
+        public IHttpActionResult PostAdress(Adress adress)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Adresses.Add(adress);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = adress.Id }, adress);
+        }
+
+        // DELETE: api/Adresses/5
+        [ResponseType(typeof(Adress))]
+        public IHttpActionResult DeleteAdress(int id)
+        {
+            Adress adress = db.Adresses.Find(id);
+            if (adress == null)
+            {
+                return NotFound();
+            }
+
+            db.Adresses.Remove(adress);
+            db.SaveChanges();
+
+            return Ok(adress);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AdressExists(int id)
+        {
+            return db.Adresses.Count(e => e.Id == id) > 0;
+        }
+    }
+}
